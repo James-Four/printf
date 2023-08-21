85a11a981d06ca9b24eddbac9cfc1ad6ccc12bd1
James-Four <opiojames601@gmail.com>
2023-08-21 22:16:06 +0300

    task0

--- _printf.c
+++ _printf.c
@@ -1,52 +1,34 @@
 #include "main.h"
+
 /**
- * _printf - printf function.
- * @format: input
- * Return: length of input.
+ * _printf - implementation of the inbuilt printf
+ * @format: the format specifier
+ * Return: the formated string
  */
+
 int _printf(const char *format, ...)
 {
-	int cha_printed = 0, i = 0;
+	int printed = 0;
+
 	va_list args;
+
 	va_start(args, format);
 
-	while (format[i])
+	while (*format != '\0')
 	{
-	if (format[i] == '%')
-	{
-		i++;
-		cha_printed++;
-
-		switch (format[i])
+		if (*format == '%')
 		{
-			case 'c':
-				{
-		int x = va_arg(args, int);
-		write(1, &c, 1);
-		break;
-				}
-			case 's':
-				{
-		char *x = va_arg(args, char*);
-		int strlen = 0;
-		while(x[strlen] != '\0')
-		strlen++;
-		write(1, x, strlen);
-		cha_printed += strlen;
-		break;
-				}
-			case '%':
-				write(1, format[i], 1);
-				break;
+			format++;
+			printed = selector(format, args, printed);
+			format++;
+		}
+		else
+		{
+			_putchar(*format);
+			printed++;
+			format++;
 		}
 	}
-	else
-	{
-		write(1, format[i], 1);
-		cha_printed++;
-	}
-		i++;
-	}
 	va_end(args);
-	return (cha_printed);
+	return (printed);
 }
