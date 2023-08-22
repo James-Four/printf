ec7a6776c8ab3b049f1b48d6c6918daf6e28bed9
Prosper <prosperorjiogo1999@gmail.com>
2023-08-23 00:05:28 +0100

    tasks 0 & 1

--- _printf.c
+++ _printf.c
@@ -1,59 +1,111 @@
 #include "main.h"
 /**
- * _printf - printf function
- * @format: input
- * Return: the number of printed characters
+ *_printf - printf
+ *@format: const char pointer
+ *Description: this functions implement some functions of printf
+ *Return: num of characteres printed
  */
 int _printf(const char *format, ...)
 {
-	int printed = 0;
-	va_list args;
+	const char *string;
+	int cont = 0;
+	va_list arg;
 
-	if (format == NULL)
+	if (!format)
 		return (-1);
 
-	va_start(args, format);
+	va_start(arg, format);
+	string = format;
 
-	while (*format != '\0')
+	cont = loop_format(arg, string);
+
+	va_end(arg);
+	return (cont);
+}
+/**
+ *loop_format - loop format
+ *@arg: va_list arg
+ *@string: pointer from format
+ *Description: This function make loop tp string pointer
+ *Return: num of characteres printed
+ */
+int loop_format(va_list arg, const char *string)
+{
+	int i = 0, flag = 0, cont_fm = 0, cont = 0, check_per = 0;
+
+	while (i < _strlen((char *)string) && *string != '\0')
 	{
-		if (*format == '%')
+		char aux = string[i];
+
+		if (aux == '%')
 		{
-			format++;
-			switch (*format)
+			i++, flag++;
+			aux = string[i];
+			if (aux == '\0' && _strlen((char *)string) == 1)
+				return (-1);
+			if (aux == '\0')
+				return (cont);
+			if (aux == '%')
+			{
+				flag++;
+			} else
+			{
+				cont_fm = function_manager(aux, arg);
+				if (cont_fm >= 0 && cont_fm != -1)
+				{
+					i++;
+					aux = string[i];
+					if (aux == '%')
+						flag--;
+					cont = cont + cont_fm;
+				} else if (cont_fm == -1 && aux != '\n')
+				{
+					cont += _putchar('%');
+				}
+			}
+		}
+		check_per = check_percent(&flag, aux);
+		cont += check_per;
+		if (check_per == 0 && aux != '\0' && aux != '%')
+			cont += _putchar(aux), i++;
+		check_per = 0;
+	}
+	return (cont);
+}
+/**
+ * check_percent - call function manager
+ *@flag: value by reference
+ *@aux: character
+ *Description: This function print % pear
+ *Return: 1 if % is printed
+ */
+int check_percent(int *flag, char aux)
+{
+	int tmp_flag;
+	int cont = 0;
+
+	tmp_flag = *flag;
+	if (tmp_flag == 2 && aux == '%')
 	{
-		case 'c':
-			_putchar(va_arg(args, int));
-			printed++;
-			break;
-		case 's':
-			printed = printf_string(args, printed);
-			break;
-		case '%':
-			_putchar('%');
-			printed++;
-			break;
-		case 'i':
-		case 'd':
-			printed = printf_integer(args, printed);
-			break;
-		case 'r':
-			printed = printf_reverse(args, printed);
-			break;
-		case '0':
-			printed = printf_octal(va_arg(args, unsigned int), printed);
-			break;
-		default:
-			break;
+		_putchar('%');
+		tmp_flag = 0;
+		cont = 1;
 	}
-			format++;
-		}
-		else
-		{
-			_putchar(*format);
-			printed++;
-			format++;
-		}
-	}
-	va_end(args);
-	return (printed);
+	return (cont);
+}
+
+/**
+ * call_function_manager - call function manager
+ *@aux: character parameter
+ *@arg: va_list arg
+ *Description: This function call function manager
+ *Return: num of characteres printed
+ */
+
+int call_function_manager(char aux, va_list arg)
+{
+	int cont = 0;
+
+	cont = function_manager(aux, arg);
+	return (cont);
 }
--- main.h
+++ main.h
@@ -1,19 +1,23 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-#include <stdio.h>
+#ifndef HEADER_MAIN
+#define HEADER_MAIN
 #include <stdarg.h>
-#include <unistd.h>
-#include <limits.h>
-#include <stdlib.h>
-
-int _putchar(char character);
-int printf_char(va_list args, int printed);
-int printf_string(va_list args, int printed);
+int _putchar(char c);
+void _puts(char *str);
+int _strlen(char *s);
 int _printf(const char *format, ...);
-int print_digits(va_list args, int printed);
-int printf_integer(va_list args, int printed);
-int printf_reverse(va_list args, int printed);
-int printf_octal(unsigned int num, int printed);
-
+char *convert_to(char representation[], unsigned int num, int base);
+int function_manager(char c, va_list arg);
+int _switch(char c, va_list arg);
+int print_chars(va_list arg);
+int print_signed(va_list arg, int base);
+int print_unsigned(va_list arg, int base);
+int print_strings(va_list arg);
+int print_ptr(va_list arg);
+int print_rot13(va_list arg);
+int print_rev(va_list arg);
+int print_base16_upper_lower(va_list arg, char *representation);
+int loop_format(va_list arg, const char *string);
+int call_function_manager(char aux, va_list arg);
+int check_percent(int *flag, char aux);
 #endif
+
